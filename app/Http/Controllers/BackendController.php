45cd2bf6ba436fbe5284e62f8bc66376058b6bc1
Zhafron <zhafronadani@gmail.com>
2021-09-26 21:03:39 +0700

    Tambah Backend Controller
    Signed-off-by: Zhafron <zhafronadani@gmail.com>

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DosenAdmin;
+use Auth;
+
+class BackendController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public static function getNama()
+    {
+        $user = Auth::user(); // returns an instance of the authenticated user...
+        $id = $user->id;
+        $dosenadmin = DosenAdmin::with('user')->where('id', $id)->first();
+        $nama = $dosenadmin->nama;
+
+        return $nama;
+    }
+
+}
